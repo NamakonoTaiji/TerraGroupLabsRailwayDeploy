fbe90895b639d38b24dd371a9ccc57dff8b9e142
NamakonoTaiji <kazumizu0201@gmail.com>
2025-04-03 04:26:41 +0900

    テスト用の環境構築

--- src/test/java/com/terragrouplabs/TerraGroupLabsLandingPageApplicationTests.java
+++ src/test/java/com/terragrouplabs/TerraGroupLabsLandingPageApplicationTests.java
@@ -2,12 +2,15 @@ package com.terragrouplabs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TerraGroupLabsLandingPageApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // This test will now run with application-test.properties
+    }
 
 }
