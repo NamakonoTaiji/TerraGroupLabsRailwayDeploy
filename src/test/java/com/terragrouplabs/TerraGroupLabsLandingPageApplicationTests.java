0620dde818088505c03df534f022008caa27c2ba
NamakonoTaiji <kazumizu0201@gmail.com>
2025-04-06 06:34:31 +0900

    最終的なバグ修正バージョン

--- src/test/java/com/terragrouplabs/TerraGroupLabsLandingPageApplicationTests.java
+++ src/test/java/com/terragrouplabs/TerraGroupLabsLandingPageApplicationTests.java
@@ -2,15 +2,15 @@ package com.terragrouplabs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class TerraGroupLabsLandingPageApplicationTests {
 
     // JavaMailSender をモック化する
-    @MockBean
+    @MockitoBean
     private com.terragrouplabs.service.EmailService emailService;
 
     @Test
