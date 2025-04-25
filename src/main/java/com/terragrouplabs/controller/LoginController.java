94562451fe5709561a934ad703b36b7c41447b4a
NamakonoTaiji <kazumizu0201@gmail.com>
2025-04-25 20:25:27 +0900

    fix: セキュリティリスクであるデバッグセッションを削除

--- src/main/java/com/terragrouplabs/controller/LoginController.java
+++ src/main/java/com/terragrouplabs/controller/LoginController.java
@@ -1,14 +1,9 @@
 package com.terragrouplabs.controller;
 
-import java.util.Enumeration;
-
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import jakarta.servlet.http.HttpSession;
 
 /**
  * ログイン画面表示およびデバッグ用エンドポイントを提供するコントローラ。
@@ -26,29 +21,5 @@ public class LoginController {
         model.addAttribute("pageTitle", "ログイン - TerraGroupLabs");
         model.addAttribute("currentPage", "login");
         return "login";
-    }
-
-    /**
-     * 【デバッグ用】現在のセッション情報を表示するエンドポイント。
-     *
-     * @param session 現在の HttpSession オブジェクト (Springが注入)
-     * @return セッションIDと属性を含む文字列 (HTMLとして表示される)
-     */
-    @GetMapping("/debug-session")
-    @ResponseBody // 戻り値がビュー名ではなくレスポンスボディそのもの
-    public String debugSession(@NonNull HttpSession session, @NonNull Model model) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Session ID: ").append(session.getId()).append("<br>");
-
-        // セッション属性のキーと値を列挙
-        Enumeration<String> attrs = session.getAttributeNames();
-        while (attrs.hasMoreElements()) {
-            String name = attrs.nextElement();
-            sb.append(name).append(": ").append(session.getAttribute(name)).append("<br>");
-        }
-
-        model.addAttribute("pageTitle", "Session Debug");
-        model.addAttribute("currentPage", "debug-session");
-        return sb.toString();
     }
 }
--- src/main/java/com/terragrouplabs/service/ServiceCategoryService.java
+++ src/main/java/com/terragrouplabs/service/ServiceCategoryService.java
@@ -2,23 +2,40 @@ package com.terragrouplabs.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 import com.terragrouplabs.entity.ServiceCategory;
 import com.terragrouplabs.repository.ServiceCategoryRepository;
 
-@Service
+/**
+ * サービスカテゴリに関するビジネスロジックを担当するサービスクラスです。
+ */
+@Service // このクラスがSpringのサービスコンポーネントであることを示す
 public class ServiceCategoryService {
 
-    @Autowired
-    private ServiceCategoryRepository categoryRepository;
-    
+    // ServiceCategoryRepositoryのインスタンスをSpringが自動的に注入します。
+    private final ServiceCategoryRepository categoryRepository;
+
     /**
-     * すべてのサービスカテゴリーを取得します
-     * @return カテゴリーのリスト
+     * コンストラクタインジェクション: Springが起動時にServiceCategoryRepositoryのBeanを引数に渡して
+     * このServiceCategoryServiceのインスタンスを生成します。
+     *
+     * @param categoryRepository 注入されるリポジトリのインスタンス (NonNullでnullでないことを明示)
+     */
+    // @Autowired はコンストラクタが1つの場合は省略可能
+    public ServiceCategoryService(@NonNull ServiceCategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    /**
+     * すべてのサービスカテゴリーをデータベースから取得します。
+     *
+     * @return ServiceCategoryエンティティのリスト
      */
     public List<ServiceCategory> getAllCategories() {
+        // 注入されたリポジトリのfindAll()メソッドを呼び出し、
+        // データベースからすべてのServiceCategoryレコードを取得してリストとして返します。
         return categoryRepository.findAll();
     }
 }
